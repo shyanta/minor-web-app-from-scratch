926d7c552b8747eabfcd6596ac016d3897cb5fb6
Timo Verkroost <timo.verkroost@gmail.com>
2017-02-07 12:34:35 +0100

    Update vars in geo script

--- opdracht 4 - geo/cmdgeo-0.1.js
+++ opdracht 4 - geo/cmdgeo-0.1.js
@@ -9,24 +9,52 @@
  *   Copyleft 2012, all wrongs reversed.
  */
 
-// Variable declaration
-var SANDBOX = "SANDBOX";
-var LINEAIR = "LINEAIR";
-var GPS_AVAILABLE = 'GPS_AVAILABLE';
-var GPS_UNAVAILABLE = 'GPS_UNAVAILABLE';
-var POSITION_UPDATED = 'POSITION_UPDATED';
-var REFRESH_RATE = 1000;
+// Not used
+// var SANDBOX = "SANDBOX";
+// Gebruik: ET.addListener('foo', handleEvent); ET.fire('event_name'); ET.removeListener('foo', handleEvent);
+
 var currentPosition = currentPositionMarker = customDebugging = debugId = map = interval =intervalCounter = updateMap = false;
-var locatieRij = markerRij = [];
 
 // Event functies - bron: http://www.nczonline.net/blog/2010/03/09/custom-events-in-javascript/ Copyright (c) 2010 Nicholas C. Zakas. All rights reserved. MIT License
-// Gebruik: ET.addListener('foo', handleEvent); ET.fire('event_name'); ET.removeListener('foo', handleEvent);
-function EventTarget(){this._listeners={}}
-EventTarget.prototype={constructor:EventTarget,addListener:function(a,c){"undefined"==typeof this._listeners[a]&&(this._listeners[a]=[]);this._listeners[a].push(c)},fire:function(a){"string"==typeof a&&(a={type:a});a.target||(a.target=this);if(!a.type)throw Error("Event object missing 'type' property.");if(this._listeners[a.type]instanceof Array)for(var c=this._listeners[a.type],b=0,d=c.length;b<d;b++)c[b].call(this,a)},removeListener:function(a,c){if(this._listeners[a]instanceof Array)for(var b=
-    this._listeners[a],d=0,e=b.length;d<e;d++)if(b[d]===c){b.splice(d,1);break}}}; var ET = new EventTarget();
+
+function EventTarget(){ this._listeners={} }
+
+EventTarget.prototype = {
+    constructor: EventTarget,
+    addListener: function(a, c) {
+        "undefined" == typeof this._listeners[a] && (this._listeners[a] = []);
+        this._listeners[a].push(c)
+    },
+    fire: function(a) {
+        var c;
+
+        "string" == typeof a && (a = {
+            type: a
+        });
+        a.target || (a.target = this);
+        if (!a.type) throw Error("Event object missing 'type' property.");
+        if (this._listeners[a.type] instanceof Array)
+            for (c = this._listeners[a.type], b = 0, d = c.length; b < d; b++) c[b].call(this, a)
+    },
+    removeListener: function(a, c) {
+        var b;
+
+        if (this._listeners[a] instanceof Array)
+            for ( b =
+                this._listeners[a], d = 0, e = b.length; d < e; d++)
+                if (b[d] === c) {
+                    b.splice(d, 1);
+                    break
+                }
+    }
+};
+var ET = new EventTarget();
 
 // Test of GPS beschikbaar is (via geo.js) en vuur een event af
 function init(){
+    var GPS_AVAILABLE = 'GPS_AVAILABLE';
+    var GPS_UNAVAILABLE = 'GPS_UNAVAILABLE';
+
     debug_message("Controleer of GPS beschikbaar is...");
 
     ET.addListener(GPS_AVAILABLE, _start_interval);
@@ -37,6 +65,9 @@ function init(){
 
 // Start een interval welke op basis van REFRESH_RATE de positie updated
 function _start_interval(event){
+    var POSITION_UPDATED = 'POSITION_UPDATED';
+    var REFRESH_RATE = 1000;
+
     debug_message("GPS is beschikbaar, vraag positie.");
     _update_position();
     interval = self.setInterval(_update_position, REFRESH_RATE);
@@ -59,8 +90,11 @@ function _set_position(position){
 // Controleer de locaties en verwijs naar een andere pagina als we op een locatie zijn
 function _check_locations(event){
     // Liefst buiten google maps om... maar helaas, ze hebben alle coole functies
-    for (var i = 0; i < locaties.length; i++) {
-        var locatie = {coords:{latitude: locaties[i][3],longitude: locaties[i][4]}};
+    var i;
+    var locatie;
+
+    for (i = 0; i < locaties.length; i++) {
+        locatie = {coords:{latitude: locaties[i][3],longitude: locaties[i][4]}};
 
         if(_calculate_distance(locatie, currentPosition)<locaties[i][2]){
 
@@ -105,13 +139,21 @@ function _calculate_distance(p1, p2){
  */
 function generate_map(myOptions, canvasId){
 // TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
+
+    var LINEAIR = "LINEAIR";
+    var locatieRij = markerRij = [];
+    var routeList = [];
+    var i;
+    var markerLatLng;
+    var attr;
+    var marker;
+
     debug_message("Genereer een Google Maps kaart en toon deze in #"+canvasId)
     map = new google.maps.Map(document.getElementById(canvasId), myOptions);
 
-    var routeList = [];
     // Voeg de markers toe aan de map afhankelijk van het tourtype
     debug_message("Locaties intekenen, tourtype is: "+tourType);
-    for (var i = 0; i < locaties.length; i++) {
+    for ( i = 0; i < locaties.length; i++) {
 
         // Met kudos aan Tomas Harkema, probeer local storage, als het bestaat, voeg de locaties toe
         try {
@@ -120,16 +162,16 @@ function generate_map(myOptions, canvasId){
             debug_message("Localstorage kan niet aangesproken worden: "+error);
         }
 
-        var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
+        markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
         routeList.push(markerLatLng);
 
         markerRij[i] = {};
-        for (var attr in locatieMarker) {
+        for (attr in locatieMarker) {
             markerRij[i][attr] = locatieMarker[attr];
         }
         markerRij[i].scale = locaties[i][2]/3;
 
-        var marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             position: markerLatLng,
             map: map,
             icon: markerRij[i],

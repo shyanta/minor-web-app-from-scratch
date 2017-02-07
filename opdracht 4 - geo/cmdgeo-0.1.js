14fd85d1d152bd0a4d478b61903c81b93ba142af
Timo Verkroost <timo.verkroost@gmail.com>
2017-02-07 14:31:55 +0100

    Update geo scripts

--- opdracht 4 - geo/cmdgeo-0.1.js
+++ opdracht 4 - geo/cmdgeo-0.1.js
@@ -48,42 +48,50 @@
     };
     var ET = new EventTarget();
 
-    // Test of GPS beschikbaar is (via geo.js) en vuur een event af
-    function init(){
-        var gpsAvailable = "GPS_AVAILABLE";
-        var gpsUnavailable = "GPS_UNAVAILABLE";
-
-        debug_message("Controleer of GPS beschikbaar is...");
-
-        ET.addListener(gpsAvailable, _start_interval);
-        ET.addListener(gpsUnavailable, function(){debug_message("GPS is niet beschikbaar.")});
-
-        (geo_position_js.init())?ET.fire(gpsAvailable):ET.fire(gpsUnavailable);
-    }
-
     // Start een interval welke op basis van refreshRate de positie updated
-    function _start_interval(event){
-        var positionUpdated = "POSITION_UPDATED";
-        var refreshRate = 1000;
+    var checkInterval = {
+        positionUpdated: "POSITION_UPDATED",
+        refreshRate: 1000,
+        startInterval: function () {
+            // debug_message("GPS is beschikbaar, vraag positie.");
+            checkPosition.updatePosition();
+            interval = self.setInterval(checkPosition.updatePosition, this.refreshRate);
+            ET.addListener(this.positionUpdated, _check_locations);
+        }
+    };
 
-        debug_message("GPS is beschikbaar, vraag positie.");
-        _update_position();
-        interval = self.setInterval(_update_position, refreshRate);
-        ET.addListener(positionUpdated, _check_locations);
-    }
+    // Test of GPS beschikbaar is (via geo.js) en vuur een event af
+    var checkGps = {
+        gpsAvailable: "GPS_AVAILABLE",
+        gpsUnavailable: "GPS_UNAVAILABLE",
+        gpsInit: function () {
+            // debug_message("Controleer of GPS beschikbaar is...");
+
+            ET.addListener(this.gpsAvailable, checkInterval.startInterval);
+            ET.addListener(this.gpsUnavailable, function(){
+                debug_message("GPS is niet beschikbaar.")
+            });
+
+            (geo_position_js.init())?ET.fire(this.gpsAvailable):ET.fire(this.gpsUnavailable);
+        }
+    };
 
     // Vraag de huidige positie aan geo.js, stel een callback in voor het resultaat
-    function _update_position(){
-        intervalCounter++;
-        geo_position_js.getCurrentPosition(_set_position, _geo_error_handler, {enableHighAccuracy:true});
-    }
+    var checkPosition = {
+        updatePosition: function () {
+            intervalCounter++;
+            geo_position_js.getCurrentPosition(checkSetPosition.setPosition(), _geo_error_handler, {enableHighAccuracy:true});
+        }
+    };
 
     // Callback functie voor het instellen van de huidige positie, vuurt een event af
-    function _set_position(position){
-        currentPosition = position;
-        ET.fire("POSITION_UPDATED");
-        debug_message(intervalCounter+" positie lat:"+position.coords.latitude+" long:"+position.coords.longitude);
-    }
+    var checkSetPosition = {
+        setPosition: function (position) {
+            currentPosition = position;
+            ET.fire("POSITION_UPDATED");
+            debug_message(intervalCounter+" positie lat:"+position.coords.latitude+" long:"+position.coords.longitude);
+        }
+    };
 
     // Controleer de locaties en verwijs naar een andere pagina als we op een locatie zijn
     function _check_locations(event){
@@ -94,7 +102,7 @@
         for (i = 0; i < locaties.length; i++) {
             locatie = {coords:{latitude: locaties[i][3],longitude: locaties[i][4]}};
 
-            if(_calculate_distance(locatie, currentPosition)<locaties[i][2]){
+            if(checkCalDistance.calculateDistance(locatie, currentPosition)<locaties[i][2]){
 
                 // Controle of we NU op die locatie zijn, zo niet gaan we naar de betreffende page
                 if(window.location!=locaties[i][1] && localStorage[locaties[i][0]]=="false"){
@@ -115,13 +123,25 @@
     }
 
     // Bereken het verchil in meters tussen twee punten
-    function _calculate_distance(p1, p2){
-        var pos1 = new google.maps.LatLng(p1.coords.latitude, p1.coords.longitude);
-        var pos2 = new google.maps.LatLng(p2.coords.latitude, p2.coords.longitude);
-        return Math.round(google.maps.geometry.spherical.computeDistanceBetween(pos1, pos2), 0);
-    }
+    var checkCalDistance = {
+        pos1: new google.maps.LatLng(p1.coords.latitude, p1.coords.longitude),
+        pos2: new google.maps.LatLng(p2.coords.latitude, p2.coords.longitude),
+        calculateDistance: function (p1, p2) {
+            return Math.round(google.maps.geometry.spherical.computeDistanceBetween(this.pos1, this.pos2), 0);
+        }
+    };
 
+    checkInterval.startInterval();
+    checkGps.gpsInit();
+    checkPosition.updatePosition();
+    checkSetPosition.setPosition();
+    checkCalDistance.calculateDistance();
 
+    console.log(checkInterval.startInterval);
+}());
+
+// IIFE GMAPS
+(function(){
     // GOOGLE MAPS FUNCTIES
     /**
      * generate_map(myOptions, canvasId)

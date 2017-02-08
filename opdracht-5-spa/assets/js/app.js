2360b6295016dff53bbe5108c1fab66967c5103c
Timo Verkroost <timo.verkroost@gmail.com>
2017-02-08 13:49:59 +0100

    Assignment 5 done

--- opdracht-5-spa/assets/js/app.js
+++ opdracht-5-spa/assets/js/app.js
@@ -1,1 +1,52 @@
+(function(){
+    "use strict";
+    // App defined
+    var app = {
+        init: function () {
+            // get routers
+            routes.init();
+        }
+    };
+    // Routes
+    var routes = {
+        init: function () {
+            var location = window.location;
+            var oldHash = location.hash;
+            var newHash = location.hash;
+            // default section toggle
+            sections.toggle(newHash, oldHash);
+            window.onhashchange = function () {
+                newHash = location.hash;
+                sections.toggle(newHash, oldHash);
+                // Update hash after toggle
+                oldHash = newHash;
+            };
+        }
+    };
+    // Sections
+    var sections = {
+        toggle: function (newRoute, oldRoute) {
+            // Default section
+            if (!oldRoute) {
+                newRoute = "#startScreen";
+            }
+            // If is empty do not run
+            if (oldRoute) {
+                // Remove hash for selector
+                oldRoute = oldRoute.replace("#", "");
+                var hideSection = document.getElementById(oldRoute);
+                hideSection.classList.remove("active");
+            }
+            // If is empty do not run
+            if (newRoute) {
+                // Remove hash for selector
+                newRoute = newRoute.replace("#", "");
+                var showSection = document.getElementById(newRoute);
+                showSection.classList.add("active");
+            }
 
+        }
+    };
+    // Run app
+    app.init();
+}());

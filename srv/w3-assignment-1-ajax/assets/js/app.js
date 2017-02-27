a3744934bc5c41fde1939f2f79bd4f7ed8d5b0a0
Timo Verkroost <timo.verkroost@gmail.com>
2017-02-27 18:09:03 +0100

    Filter for regions build

--- srv/w3-assignment-1-ajax/assets/js/app.js
+++ srv/w3-assignment-1-ajax/assets/js/app.js
@@ -7,14 +7,14 @@
 
     var request = {
         countries: function () {
-            var apiURL = "https://restcountries.eu/rest/v1/all";
+            var apiURL = "https://restcountries.eu/rest/v2/all";
             aja()
                 .method('get')
                 .url(apiURL)
                 .on('200', function (response) {
                     dataStore = response;
-                    // Load app when have data
-                    app.init();
+                    // Start routers
+                    routers.listen();
                     // Remove loader
                     loader.remove();
                     // Fade in response
@@ -22,19 +22,18 @@
                 })
                 .on('40x', function (response) {
                     console.log(response);
+                    routers.failed();
                 })
                 .on('500', function (response) {
                     console.log(response);
+                    routers.failed();
                 })
                 .go();
         }
     };
-    // Request the API
-    request.countries();
 
     var app = {
         init: function () {
-            console.log(dataStore);
             // Get hash from url
             var route = window.location.hash;
             // If no hash is in the URL add default hash
@@ -42,13 +41,12 @@
                 route = "#countries";
                 window.location.href  = window.location.href + route
             }
-            // Start routers
-            routers.init();
+            request.countries();
         }
     };
 
     var routers ={
-        init: function () {
+        listen: function () {
             // Routers
             routie({
                 'countries': function() {
@@ -68,6 +66,9 @@
                     document.getElementById("country").classList.add("active");
                 }
             });
+        },
+        failed: function () {
+            console.log("fail");
         }
     };
 
@@ -83,10 +84,10 @@
             }
         },
         overviewCountries: function () {
-            // Get all country names
-            var countries = dataStore.map(function(objectItem) {
-                return { name: objectItem.name,  countryCode: objectItem.alpha3Code };
-            });
+            filters.regionCountryButtons();
+            // Default render with all regions
+            var filteredRegion = dataStore;
+
             // Render country name and link with alpha3Code
             var countrySingle = {
                 country: {
@@ -97,42 +98,132 @@
                         return this.name;
                     },
                     href: function() {
-                        return window.location.href + '/' + this.countryCode.toLowerCase();
+                        return window.location.href + '/' + this.alpha3Code;
                     }
                 }
             };
             // Render HTML
-            Transparency.render(document.getElementById("countriesOverview"), countries, countrySingle);
-            Transparency.render(document.getElementById("countriesSearch"), countries, countrySingle);
+            Transparency.render(document.getElementById("countriesOverview"), filteredRegion, countrySingle);
+            Transparency.render(document.getElementById("countriesSearch"), filteredRegion, countrySingle);
+
+            // Select region with the filters
+            document.getElementById("allClickButtons").addEventListener("click",function (e) {
+                filters.regionActive(e);
+            });
         },
         singulairCountries: function () {
             // Get the right country by hash
             var initialPage = window.location.hash;
-            var countryLink = initialPage.split('/')[1].toUpperCase();
+            var countryLink = initialPage.split('/')[1];
             // Filter the selected country
             var singleCountry = dataStore.filter(function(value) {
                 return value.alpha3Code == countryLink;
             });
-            console.log(singleCountry);
             Transparency.render(document.getElementById('countryContainer'), singleCountry);
             // Google maps update
             var latValue = singleCountry[0].latlng[0];
             var lngValue = singleCountry[0].latlng[1];
-            this.maps(latValue, lngValue);
+            this.googleMaps(latValue, lngValue);
+
+            var x = singleCountry.reduce(function(object) {
+                return object.region + object.region;
+            }, 'Summery');
+            console.log(x);
         },
-        maps: function (lat, lng) {
-            var locationPoint = {lat: lat, lng: lng};
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: locationPoint,
+        googleMaps: function (lat, lng) {
+            // Needed to reload Google Maps, because if the marker updates it needed a redraw.
+            var latlng = new google.maps.LatLng(lat, lng);
+            var mapOptions = {
+                center: latlng,
+                zoom: 3,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
                 disableDefaultUI: true
-            });
+            };
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
             var marker = new google.maps.Marker({
-                position: locationPoint,
+                position: latlng,
                 map: map
             });
         }
     };
+
+    var filters = {
+        regionCountryButtons: function () {
+            var regions = dataStore.map(function(objectItem) {
+                return { region: objectItem.region };
+            });
+
+            var regionArray = [], regionOutput = [], l = regions.length, i;
+            for( i=0; i<l; i++) {
+                if( regionArray[regions[i].region]) continue;
+                regionArray[regions[i].region] = true;
+                // Do not add empty values
+                if(regions[i].region != "") {
+                    regionOutput.push(regions[i].region);
+                }
+            }
+            // Render regions for filter
+            var filterButtons = {
+                    region: {
+                        text: function() {
+                            return this.value;
+                        },
+                        value: function() {
+                            return this.value;
+                        },
+                        for: function() {
+                            return this.value;
+                        }
+                    },
+                    regionGroup: {
+                        value: function() {
+                            return this.value;
+                        },
+                        id: function() {
+                            return this.value;
+                        }
+                    }
+
+            };
+            // Render HTML
+            Transparency.render(document.getElementById("filterButtons"), regionOutput, filterButtons);
+        },
+        regionActive: function (e) {
+            if (e.target && e.target.matches(".regionRadio")) {
+                // Filter on region
+
+                var filteredRegion;
+                if (e.target.value == "all") {
+                    filteredRegion = dataStore;
+                } else {
+                    filteredRegion = dataStore.filter(function(value) {
+                        return value.region == e.target.value;
+                    });
+                }
+
+                // Render country name and link with alpha3Code
+                var countrySingle = {
+                    country: {
+                        text: function() {
+                            return this.name;
+                        },
+                        value: function() {
+                            return this.name;
+                        },
+                        href: function() {
+                            return window.location.href + '/' + this.alpha3Code;
+                        }
+                    }
+                };
+                // Render HTML
+                Transparency.render(document.getElementById("countriesOverview"), filteredRegion, countrySingle);
+                Transparency.render(document.getElementById("countriesSearch"), filteredRegion, countrySingle);
+            }
+        }
+    };
+
+    app.init();
 
     /* STRUCTURE
     Ajax load countries API
